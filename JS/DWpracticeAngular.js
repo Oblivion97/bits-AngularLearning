fd662902838f182c2a15970aa286c9bed0f72446
H M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-04-24 23:21:30 +0600

    Angular Directives

---
+++ JS/DWpracticeAngular.js
@@ -0,0 +1,8 @@
+function SimpleController($scope) {
+    $scope.customersController = [{namec:'James Hoog',city:'New York'},
+                            {namec:'Nail Knite',city:'Paris'},
+                            {namec:'Pit Alex',city:'London'},
+                            {namec:'Mc Lyon',city:'Paris'},
+                            {namec:'Paul Adam',city:'Rome'},
+                            {namec:'Lauson Hen',city:'San Jose'}];
+}
