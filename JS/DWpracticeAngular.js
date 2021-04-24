1fc4d4a65ae4f45666eaa493576052f49d547689
H M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-04-25 00:13:51 +0600

    scope controller problem solve

--- JS/DWpracticeAngular.js
+++ JS/DWpracticeAngular.js
@@ -1,8 +1,10 @@
-function SimpleController($scope) {
-    $scope.customers = [{namec:'James Hoog',city:'New York'},
+var app = angular.module('myAngularApp', []);
+	app.controller('SimpleController', function($scope) {
+	    $scope.customers = [{namec:'James Hoog',city:'New York'},
                             {namec:'Nail Knite',city:'Paris'},
                             {namec:'Pit Alex',city:'London'},
                             {namec:'Mc Lyon',city:'Paris'},
                             {namec:'Paul Adam',city:'Rome'},
                             {namec:'Lauson Hen',city:'San Jose'}];
-}
+		});
+	
