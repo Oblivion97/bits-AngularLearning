38f1595810d6d117a78fb2d1a81b3fc225f469fd
H M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-04-25 12:50:07 +0600

    add new object

--- JS/DWpracticeAngular.js
+++ JS/DWpracticeAngular.js
@@ -1,10 +1,22 @@
 var app = angular.module('myAngularApp', []); //empty array is for dependacy injection
-	app.controller('SimpleController', function($scope) {
-	    $scope.customers = [{namec:'James Hoog',city:'New York'},
-                            {namec:'Nail Knite',city:'Paris'},
-                            {namec:'Pit Alex',city:'London'},
-                            {namec:'Mc Lyon',city:'Paris'},
-                            {namec:'Paul Adam',city:'Rome'},
-                            {namec:'Lauson Hen',city:'San Jose'}];
-		});
+
+
+app.controller('SimpleController', function($scope) {
+	$scope.customers = 
+        [{namec:'James Hoog',city:'New York'},
+        {namec:'Nail Knite',city:'Paris'},
+        {namec:'Pit Alex',city:'London'},
+        {namec:'Mc Lyon',city:'Paris'},
+        {namec:'Paul Adam',city:'Rome'},
+        {namec:'Lauson Hen',city:'San Jose'}];
+
+        $scope.addCustomer = function(){
+            $scope.customers.push({
+                    namec:$scope.namec,
+                    city:$scope.city
+                });
+        }
+        
+	});
+
 	
