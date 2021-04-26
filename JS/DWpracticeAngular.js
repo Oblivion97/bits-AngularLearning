6675d544819969210f7e8da781ebf90af1317da2
H M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-04-26 18:03:50 +0600

    custom service making problem

--- JS/DWpracticeAngular.js
+++ JS/DWpracticeAngular.js
@@ -48,12 +48,10 @@ app.controller('httpController', function ($scope, $http) {
 
 app.controller('timeoutController', function ($scope, $timeout) {
         $scope.myHeader = "Are you watching closely?";
-
-        function better() {
+        $timeout(function () {
                 $scope.myHeader = "Every magic trick consists of three parts: PLEDGE, TURN and PRESTIGE.";
-                better();
-        }
-        $timeout(better, 2000);
+
+        }, 2000);
 });
 
 app.controller('intervalController', function ($scope, $interval) {
@@ -61,4 +59,26 @@ app.controller('intervalController', function ($scope, $interval) {
         $interval(function () {
                 $scope.theTime = new Date().toLocaleTimeString();
         }, 1000);
+});
+
+app.service('HEX', function () {
+        this.myFunc = function (x) {
+                return x.toString(16);
+        }
+});
+app.controller('customServiceHex', function ($scope, HEX) {
+        var a = Number($scope.number);
+        $scope.NumbertoHex = function () {
+                $scope.intTOhex = HEX.myFunc(a);
+        }
+         
+         
+});
+
+app.controller('AddNumbersController', function ($scope) {
+        $scope.AddNumbers = function () {
+                var a = Number($scope.a);
+                var b = Number($scope.b);
+                $scope.sum = a + b;
+        }
 });
