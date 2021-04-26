4e939a542a30957baa0f4e40d15c4f9b10e56230
H M Mahmudul Hasan <hhasan152041@bscse.uiu.ac.bd>
2021-04-26 13:06:34 +0600

    builtin service examples

--- JS/DWpracticeAngular.js
+++ JS/DWpracticeAngular.js
@@ -1,22 +1,64 @@
 var app = angular.module('myAngularApp', []); //empty array is for dependacy injection
 
 
-app.controller('SimpleController', function($scope) {
-	$scope.customers = 
-        [{namec:'James Hoog',city:'New York'},
-        {namec:'Nail Knite',city:'Paris'},
-        {namec:'Pit Alex',city:'London'},
-        {namec:'Mc Lyon',city:'Paris'},
-        {namec:'Paul Adam',city:'Rome'},
-        {namec:'Lauson Hen',city:'San Jose'}];
+app.controller('SimpleController', function ($scope) {
+        $scope.customers = [{
+                        namec: 'James Hoog',
+                        city: 'New York'
+                },
+                {
+                        namec: 'Nail Knite',
+                        city: 'Paris'
+                },
+                {
+                        namec: 'Pit Alex',
+                        city: 'London'
+                },
+                {
+                        namec: 'Mc Lyon',
+                        city: 'Paris'
+                },
+                {
+                        namec: 'Paul Adam',
+                        city: 'Rome'
+                },
+                {
+                        namec: 'Lauson Hen',
+                        city: 'San Jose'
+                }
+        ];
 
-        $scope.addCustomer = function(){
-            $scope.customers.push({
-                    namec:$scope.namec,
-                    city:$scope.city
+        $scope.addCustomer = function () {
+                $scope.customers.push({
+                        namec: $scope.namec,
+                        city: $scope.city
                 });
         }
-        
-	});
 
-	
+});
+app.controller('locationController', function ($scope, $location) { //location built in service
+        $scope.myUrl = $location.absUrl();
+});
+
+app.controller('httpController', function ($scope, $http) {
+        $http.get("/HTML/usedHtmlPages/welcome.html").then(function (response) {
+                $scope.httpResponse = response.data;
+        });
+});
+
+app.controller('timeoutController', function ($scope, $timeout) {
+        $scope.myHeader = "Are you watching closely?";
+
+        function better() {
+                $scope.myHeader = "Every magic trick consists of three parts: PLEDGE, TURN and PRESTIGE.";
+                better();
+        }
+        $timeout(better, 2000);
+});
+
+app.controller('intervalController', function ($scope, $interval) {
+        $scope.theTime = new Date().toLocaleTimeString();
+        $interval(function () {
+                $scope.theTime = new Date().toLocaleTimeString();
+        }, 1000);
+});
